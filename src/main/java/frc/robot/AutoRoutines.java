6f11b155cb5065ae91c754008f1b5437f22d459a
NeoLoonz <madisonlunna@icloud.com>
2025-02-23 19:59:34 -0500

    Paths and Command Import
    Import to allow Commands.sequence()
    Tetoris, Bermuda, and Tetoris+Bermuda Autonomous Choreo path code.

--- src/main/java/frc/robot/AutoRoutines.java
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -3,6 +3,8 @@ package frc.robot;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class AutoRoutines {
     private final AutoFactory m_factory;
@@ -18,6 +20,46 @@ public class AutoRoutines {
         routine.active().onTrue(
             simplePath.resetOdometry()
                 .andThen(simplePath.cmd())
+        );
+        return routine;
+    }
+
+    
+    public AutoRoutine TetorisAuto() {
+        final AutoRoutine routine = m_factory.newRoutine("Tetoris Auto");
+        final AutoTrajectory Tetoris = routine.trajectory("Tetoris");
+
+        routine.active().onTrue(
+            Tetoris.resetOdometry()
+                .andThen(Tetoris.cmd())
+        );
+        return routine;
+    }
+
+    public AutoRoutine BermudaAuto() {
+        final AutoRoutine routine = m_factory.newRoutine("Bermuda Auto");
+        final AutoTrajectory Bermuda = routine.trajectory("Bermuda");
+
+        routine.active().onTrue(
+            Bermuda.resetOdometry()
+                .andThen(Bermuda.cmd())
+        );
+        return routine;
+    }
+
+    public AutoRoutine TetorisANDBermudaAuto() {
+        final AutoRoutine routine = m_factory.newRoutine("TetorisANDBermuda Auto");
+        final AutoTrajectory Tetoris = routine.trajectory("Tetoris");
+        final AutoTrajectory Bermuda = routine.trajectory("Bermuda");
+
+  
+        routine.active().onTrue(
+        Commands.sequence(
+            Tetoris.resetOdometry(),
+            Tetoris.cmd(),
+            Bermuda.cmd()
+            
+            )
         );
         return routine;
     }
