f93d078427fe4b70ceccf04f317712e5b00df88b
cars-chaos <carleighejohnson@icloud.com>
2025-02-25 20:00:36 -0500

    First Climber code

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -3,16 +3,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.Climber;
 
+package frc.robot;
+import edu.wpi.first.wpilibj2.subsystems.ClimberConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.controls.ControlRequest;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
   private TalonFX climberMotor;
   public Climber() {
     climberMotor = new TalonFX(ClimberConstants.motorID, ClimberConstants.bus)
-    TalonFXConfiguration climberConfigs = new ClimberConstants.TalonFXConfiguration
   }
 
   public void setAngle(double angleDeg){
--- src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -2,15 +2,24 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.Climber;
+
+package frc.robot;
 
 /** Add your docs here. */
 public class ClimberConstants {
-    public static int motorID = 46;
-    string bus = "rio";
-    TalonFXConfiguration config = new TalonFXConfiguration()
-                        .withMotorOutput(
-                            new MotorOutputConfigs()
-                            .withNeutralModel(NeutralMode.Brake)
-                        );
+     public static int motorID = 46;
+
+     
+//    TalonFX( 46 ,"rio")
+//    motor.apply(config);
+//     TalonFXConfiguration config = new TalonFXConfiguration()
+//     //                    .withMotorOutput(
+//    //                         new MotorOutputConfigs()
+//    //                         .withNeutralModel(NeutralMode.Brake)
+//    //                     );
+//    DutyCycleOutput(0)
+//     void setOpenLoop(90){
+//         DutyCycle.withOutput(90)
+//         motor.applyControl(Dutycycle);
+//     }
 }
