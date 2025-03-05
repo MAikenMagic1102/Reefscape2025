92ef310b1bc67342c5862845e593ab41e4da8b22
NeoLoonz <madisonlunna@icloud.com>
2025-03-05 18:58:17 -0500

    Update ClimberConstants.java

--- src/main/java/frc/robot/ClimberConstants.java
+++ src/main/java/frc/robot/ClimberConstants.java
@@ -46,7 +46,6 @@ public static double armMass = Units.lbsToKilograms(0);
 public static double armMinAngle = Units.degreesToRadians(0);
 public static double armMaxAngle = Units.degreesToRadians(0);
 public static double armStartingAngle = Units.degreesToRadians(0.0);
-public double angleDeg = Units.degreesToRadians(armGearing);
 
 public static double positionTolerence = 0;
 
