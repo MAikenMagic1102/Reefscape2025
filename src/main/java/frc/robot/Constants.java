52816ed235bf1eac5f63598a5deecdb8d1f450cd
NeoLoonz <madisonlunna@icloud.com>
2025-03-31 17:58:39 -0400

    Constants
    Constants needed for the AutoRunToCoral class.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,9 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public class Constants {
+  
 
     public static enum Mode {
         /** Running on a real robot. */
@@ -25,5 +28,30 @@ public class Constants {
 
       public static final String canivore = "can2";
       public static double robotArmCenterOffset = Units.inchesToMeters(1.75);
+
+ 
       public static final double robotToReefOffset = 0.62;
+
+
+
+
+
+      public static final class Limelight{
+        public static final double limelight_kP = 0.0275;
+        public static final double min_command = 0.05;
+
+            public static class FieldConstants{
+        public static final Pose2d BLUE_Reef = new Pose2d(Units.inchesToMeters(-1.5+12), Units.inchesToMeters(218.42), new Rotation2d(0));
+        public static final Pose2d RED_Reef = new Pose2d(Units.inchesToMeters(652.73-12), Units.inchesToMeters(218.42), new Rotation2d(Math.PI));
+        public static final double BLUE_AUTO_PENALTY_LINE = 8.6; // X distance from origin to center of the robot almost fully crossing the midline
+        public static final double RED_AUTO_PENALTY_LINE = 8; // X distance from origin to center of the robot almost fully crossing the midline
+
+        public static final double Coral_DIAMETER = 14; // Outer diameter of note
+
+    }
+
+  
+
+    }
+
 }
